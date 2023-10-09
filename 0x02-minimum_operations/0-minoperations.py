510f251d3a1ff4f09cf5186775b5ff498f9d2846
ndiritu-prisca <priscandiritu@outlook.com>
2023-10-10 01:22:34 +0300

    Check pycodestyle

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,6 +3,10 @@
 
 
 def minOperations(n):
+    '''
+    Function that that calculates the fewest number of operations needed to
+    result in exactly n H characters in the file.
+    '''
     if n == 1:
         return 0
 
